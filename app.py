422542c78e2ef3ff718d954018e661b339428989
Leviathanlzx <Leviathanlzx>
2023-06-20 00:05:47 +0800

    changed

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask,render_template
 app=Flask(__name__)
 @app.route("/")
 def hello_world():
-    return render_template("home.html")
+    return render_template("csshome.html")
 
 if __name__=="__main__":
     app.run(host="localhost",debug=True)
